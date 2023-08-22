c4e1bee2279795ab93caca3983994ad726383b9f
ng <53146405+nikitattt@users.noreply.github.com>
2023-08-22 22:35:03 +0300

    Remove submit proposal button

--- apps/web/src/modules/dao/components/Activity/Activity.tsx
+++ apps/web/src/modules/dao/components/Activity/Activity.tsx
@@ -1,11 +1,8 @@
-import { useConnectModal } from '@rainbow-me/rainbowkit'
-import { Button, Flex, Text } from '@zoralabs/zord'
+import { Flex, Text } from '@zoralabs/zord'
 import { useRouter } from 'next/router'
 import React from 'react'
 import useSWR from 'swr'
-import { useAccount } from 'wagmi'
 
-import { ContractButton } from 'src/components/ContractButton'
 import AnimatedModal from 'src/components/Modal/AnimatedModal'
 import { SuccessModalContent } from 'src/components/Modal/SuccessModalContent'
 import Pagination from 'src/components/Pagination'
@@ -14,35 +11,21 @@ import {
   ProposalsResponse,
   getProposals,
 } from 'src/data/subgraph/requests/proposalsQuery'
-import { useVotes } from 'src/hooks'
 import { usePagination } from 'src/hooks/usePagination'
-import { Upgrade, useProposalStore } from 'src/modules/create-proposal'
 import { ProposalCard } from 'src/modules/proposal'
-import { useLayoutStore } from 'src/stores'
 import { useChainStore } from 'src/stores/useChainStore'
-import {
-  delegateBtn,
-  selectDelegateBtn,
-  submitProposalBtn,
-} from 'src/styles/Proposals.css'
 import { sectionWrapperStyle } from 'src/styles/dao.css'
-import { AddressType } from 'src/typings'
 import { walletSnippet } from 'src/utils/helpers'
 
 import { useDelegate } from '../../hooks'
 import { useDaoStore } from '../../stores'
 import { CurrentDelegate } from './CurrentDelegate'
 import { DelegateForm } from './DelegateForm'
-import { MobileMenu } from './MobileMenu'
 import { Treasury } from './Treasury'
 
 export const Activity: React.FC = () => {
   const addresses = useDaoStore((state) => state.addresses)
-  const { createProposal } = useProposalStore()
-  const { address } = useAccount()
-  const { query, isReady, push } = useRouter()
-  const { isMobile } = useLayoutStore()
-  const { openConnectModal } = useConnectModal()
+  const { query, isReady } = useRouter()
   const chain = useChainStore((x) => x.chain)
   const LIMIT = 20
 
@@ -55,13 +38,6 @@ export const Activity: React.FC = () => {
 
   const { handlePageBack, handlePageForward } = usePagination(data?.pageInfo?.hasNextPage)
 
-  const { isOwner, isDelegating, hasThreshold, proposalVotesRequired } = useVotes({
-    chainId: chain.id,
-    governorAddress: addresses?.governor,
-    signerAddress: address,
-    collectionAddress: query?.token as AddressType,
-  })
-
   const [
     { viewCurrentDelegate, viewDelegateForm, viewSuccessfulDelegate, newDelegate },
     { view, edit, update, close },
@@ -70,16 +46,6 @@ export const Activity: React.FC = () => {
     viewDelegateForm: false,
     viewSuccessfulDelegate: false,
   })
-
-  const handleProposalCreation = () => {
-    createProposal({
-      title: undefined,
-      summary: undefined,
-      disabled: false,
-      transactions: [],
-    })
-    push(`/dao/${query.network}/${query.token}/proposal/create`)
-  }
 
   if (!data && !error) {
     return null
@@ -90,81 +56,6 @@ export const Activity: React.FC = () => {
       <Flex direction={'column'} className={sectionWrapperStyle['proposals']} mx={'auto'}>
         <Treasury />
 
-        <Flex width={'100%'} justify={'space-between'} align={'center'}>
-          <Text variant="heading-sm" style={{ fontWeight: 800 }}>
-            Proposals
-          </Text>
-
-          <Flex justify={'center'} align={'center'}>
-            {address && !isDelegating && !isOwner && !isMobile && (
-              <Flex mr={'x4'} color={'tertiary'}>
-                You have no votes.
-              </Flex>
-            )}
-            {isDelegating && !isMobile && (
-              <Flex mr={'x4'} color={'tertiary'}>
-                Your votes are delegated.
-              </Flex>
-            )}
-            {isOwner && !hasThreshold && !isMobile && (
-              <Flex mr={'x4'} color={'tertiary'}>
-                {proposalVotesRequired} votes required to propose.
-              </Flex>
-            )}
-            {isOwner || isDelegating ? (
-              <>
-                {!isMobile ? (
-                  <ContractButton
-                    className={delegateBtn}
-                    color="ghost"
-                    borderColor="border"
-                    borderStyle="solid"
-                    borderWidth="normal"
-                    handleClick={view}
-                    mr="x2"
-                  >
-                    Delegate
-                  </ContractButton>
-                ) : (
-                  <MobileMenu>
-                    <ContractButton
-                      className={selectDelegateBtn}
-                      style={{ backgroundColor: '#FFF', color: '#000' }}
-                      handleClick={view}
-                    >
-                      Delegate
-                    </ContractButton>
-                  </MobileMenu>
-                )}
-              </>
-            ) : null}
-            {!address ? (
-              <Button
-                className={submitProposalBtn}
-                onClick={openConnectModal}
-                color={'tertiary'}
-              >
-                Submit {!isMobile ? 'proposal' : null}
-              </Button>
-            ) : (
-              <Button
-                className={submitProposalBtn}
-                onClick={address ? handleProposalCreation : openConnectModal}
-                disabled={address ? !hasThreshold : false}
-                color={'tertiary'}
-              >
-                Submit {!isMobile ? 'proposal' : null}
-              </Button>
-            )}
-          </Flex>
-        </Flex>
-        {addresses && (
-          <Upgrade
-            collection={query.token as string}
-            hasThreshold={hasThreshold}
-            addresses={addresses}
-          />
-        )}
         <Flex direction={'column'} mt={'x6'}>
           {data?.proposals?.length ? (
             data?.proposals?.map((proposal, index: number) => (
